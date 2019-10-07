516b126accc2836c08e9dcc23ed4cd6e409a9728
uynet <highsate@gmail.com>
2019-10-07 22:02:10 +0900

    Revert "DB関連のAPIを写した"
    This reverts commit 3092ad8e928bf717ed9723a0bda110f3ca6a0df3.

--- app.js
+++ app.js
@@ -2,38 +2,55 @@ const bodyParser = require('body-parser') // body-parser
 const express = require("express");
 const app = express();
 const f = require('util').format;
-const passport = require('passport');
-const corser = require("corser");//CORSをなんとかするやつ
-const TwitterStrategy = require('passport-twitter'); 
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+const corser = require("corser");//CORSをなんとかするやつ
 
 const MongoClient = require("mongodb").MongoClient;
 const authMechanism = 'DEFAULT';
-const DB = require("./src/database"); 
-const database  = new DB(); 
 
 const url = 'mongodb://user:pass@localhost:27017/test';
 
+function findDB(response){
+    MongoClient.connect(url ,(error, client) => {
+      if (error) {
+        return console.dir(err);
+      }
+      const db = client.db('test');
+
+      db.collection("posts", (err, collection)=> {
+        collection.find().toArray((err, docs) => {
+          response.send(docs);
+        });
+      })
+      client.close(); 
+    }); 
+}
 
 app.use(corser.create());
 
 app.get("/",(req,res)=>{
   res.send("hello this is serverside.");
 })
-app.get("/api/countPosts",(req,res)=>{
-  database.CountPosts(res); 
-})
 app.get("/api/getPosts",(req,res)=>{
-  database.find(res);
-  //database.removeAll(); 
-})
-app.post("/api/removeAll ",(req,res)=>{
-  database.removeAll(); 
+  findDB(res); 
 })
 app.post('/api/newPost', (req, res) => {
   res.setHeader('Content-Type', 'text/plain');
   const data = req.body;
-  database.insertData(data); 
+  insertPost(data);
 })
+//postをDatabaseにinsert
+function insertPost(data){
+  MongoClient.connect(url, {useNewUrlParser: true}, function(err, client) {
+    if (err) throw err;
+    const db = client.db("test");
+
+    db.collection("posts").insertOne(data , function(err, res) {
+      if (err) throw err;
+      console.log("post",data)
+    });
+    client.close();
+  });
+}
 
 app.listen(3001);
