921883cb13accbb944a5d7cd13b2d810a3cce710
uynet <highsate@gmail.com>
2019-10-12 00:57:36 +0900

    テストデータ追加したりした

--- mongomemo.md
+++ mongomemo.md
@@ -1,8 +1,12 @@
 # mongoDBのめも
 
-- サーバーの起動
-sudo mongod --dbpath /var/lib/mongodb --logpath /var/log/mongodb.log
-
+- DBサーバーの起動
+sudo mongod --dbpath=/data
 
 - データディレクトリ
   dbpathで指定するディレクトリのことをデータディレクトリという
+  いま/dataだけどこれ以外を指定して起動するとローカルではしぬ
+  ちゃんとdockerにしたいね
+
+
+
--- src/database.js
+++ src/database.js
@@ -1,21 +1,69 @@
 const MongoClient = require("mongodb").MongoClient;
 
-const DBNAME = "test";
+const DBNAME = "palletclub";
 const COLNAME = "posts";
-const url = 'mongodb://user:pass@localhost:27017/test';
+const USERNAME = "admin";
+const PASS = "pass";
+const url = 'mongodb://'+USERNAME+':'+PASS+'@localhost:27017/'+DBNAME;
+
+const testdata = [
+  {
+    name:"アセロラ",
+    userID:"uynet",
+    date:"2019/10/10/23:59",
+    colors:"#952060,#e02070,#e0c090,#60b0b0"
+  },
+  {
+    name:"あたたかみ",
+    userID:"uynet",
+    date:"2019/10/10/23:59",
+    colors:"#c03020,#f0c0ba,#50b0c0,#303050"
+  },
+  {
+    name:"夏色",
+    userID:"uynet",
+    date:"2019/10/10/23:59",
+    colors:"#30b0d0,#faf0da,#f0c0a0,#e05070"
+  },
+  {
+    name:"さむさむ",
+    userID:"uynet",
+    date:"2019/10/10/23:59",
+    colors:"#403060,#f0c060,#50c0c0,#3080cf"
+  },
+  {
+    name:"VIVIT",
+    userID:"uynet",
+    date:"2019/10/10/23:59",
+    colors:"#f01070,#90c060,#3030a0,#f0a040"
+  },
+  {
+    name:"風鈴",
+    userID:"uynet",
+    date:"2019/10/10/23:59",
+    colors:"#45BDE2,#583F8E,#F64973,#F8DC79"
+  },
+  {
+    name:"こんにちわ",
+    userID:"uynet",
+    date:"2019/10/10/23:59",
+    colors:"#200050,#f3b000,#30a0c0,#ef1f6a"
+  },
+]
 
 module.exports = class{
   constructor(){
   }
   find(response){
+    this.insertTestDate()
     MongoClient.connect(url ,(error, client) => {
-      if (error) return console.dir(err); 
+      if (error) return console.dir(error); 
       const db = client.db(DBNAME);
 
       db.collection(COLNAME, (err, collection)=> {
         collection.find().toArray((err, docs) => {
           response.send(docs);
-          console.log(docs);
+          // console.log(docs);
         });
       })
       client.close();
@@ -23,8 +71,9 @@ module.exports = class{
   }
   CountPosts(response){
     MongoClient.connect(url, {useNewUrlParser: true}, function(err, client) {
-      const db = client.db("test");
-      db.collection("posts", (err, collection)=> {
+      const db = client.db(DBNAME
+      );
+      db.collection(COLNAME, (err, collection)=> {
         collection.find().count((err,count) => { 
           if(err)throw err;
           response.send(""+count);
@@ -35,8 +84,9 @@ module.exports = class{
   deletePost(response,data){
     MongoClient.connect(url, {useNewUrlParser: true}, function(err, client) {
       if (err) throw err;
-      const db = client.db("test");
-      db.collection("posts").remove({ID:data.ID+""});
+      const db = client.db(DBNAME
+      );
+      db.collection(COLNAME).remove({ID:data.ID+""});
       response.send("removed:"+data.ID)
       client.close();
     });
@@ -45,22 +95,44 @@ module.exports = class{
     MongoClient.connect(url, {useNewUrlParser: true}, function(err, client) {
       console.log("消えます");
       if (err) throw err;
-      const db = client.db("test");
-      db.collection("posts").remove({});
+      const db = client.db(DBNAME);
+      db.collection(COLNAME).remove({});
       client.close();
     });
   }
   insertData(data){
     MongoClient.connect(url, {useNewUrlParser: true}, function(err, client) {
       if (err) throw err;
-      const db = client.db("test");
+      const db = client.db(DBNAME);
 
 
-      db.collection("posts").insertOne(data , function(err, res) {
+      db.collection(COLNAME).insertOne(data , function(err, res) {
         if (err) throw err;
-        console.log("post",data)
+        // console.log("post",data)
       });
       client.close();
     });
+  }
+  count(response){
+    MongoClient.connect(url, {useNewUrlParser: true}, function(err, client) {
+      const db = client.db(DBNAME);
+      db.collection(COLNAME, (err, collection)=> {
+        collection.find().count((err,cnt) => { 
+          if(err)throw err;
+          return cnt;
+        });
+      })
+    });
+  }
+
+  insertTestDate(){
+    let i = 0;
+    const l = testdata.length;
+    testdata.forEach(data=>{
+      data.ID = i
+      //this.insertData(data)
+      i++;
+    })
+    //this.removeAll(); 
   }
 } 
